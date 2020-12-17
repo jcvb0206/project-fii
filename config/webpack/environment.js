30a2a898a4b991338f6c6bc8e5ac7df949169f2b
Joseph Vega <jcvb0206@gmail.com>
2020-12-17 12:34:45 -0500

    Devise added to project

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,15 @@
 const { environment } = require('@rails/webpacker');
-
 const webpack = require('webpack');
+
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default'],
   })
 );
+
 module.exports = environment;
