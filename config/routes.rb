b7b7ca1085e8fc30ff81bc7705d3304d6eee0ceb
Joseph Vega <jcvb0206@gmail.com>
2020-12-17 14:58:33 -0500

    Prototype of users view on platform

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "public#home"
+  get "/usuarios", to: "users#index"
+  get "/usuarios/:id", to: "users#show"
+  get "/registro", to: "users#create"
+  # get "/ingreso", to: "users#login"
 end
